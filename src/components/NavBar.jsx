33d40071b700a74a0c69209f96964b885caa275a
4RomanR <4rprogramming@gmail.com>
2024-02-04 01:29:32 -0400

    first frontend commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import GenresModal from './Genres/GenresModal';
+
+const NavBar = () => {
+
+    const [showGenres, setShowGenres] = useState(false);
+
+    return (
+        <>
+            <Navbar className='navbar-bg' variant="dark" expand="md">
+                <Container >
+                    <Navbar.Brand className='navbar-logo' as={Link} to="/" >
+                        <img src="/public/img/logo.jpg" alt="logo" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/">Movies</Nav.Link>
+                            <Nav.Link as={Link} to="/actors">Actors</Nav.Link>
+                            <Nav.Link as={Link} to="/directors">Directors</Nav.Link>
+                            <Nav.Link onClick={() => setShowGenres(true)}>Genres</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <GenresModal 
+                show={showGenres} 
+                handleClose={() => setShowGenres(false)} 
+            />
+        </>
+    );
+};
+
+export default NavBar;
